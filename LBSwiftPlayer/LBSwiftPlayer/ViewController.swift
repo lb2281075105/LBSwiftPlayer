9a209ff3c2b2975dbd4e9a39bf07572440913068
yunmei <yunmei@yunmeideiMac.lan>
2018-01-25 09:57:36 +0800

    初始化项目

---
+++ LBSwiftPlayer/LBSwiftPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LBSwiftPlayer
+//
+//  Created by yunmei on 2018/1/25.
+//  Copyright © 2018年 liubo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
